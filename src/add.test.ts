e21160735449a2ea642bc167af04c85c437b97f4
寻找海蓝 <18868832272@163.com>
2019-10-20 21:24:06 +0800

    feat: 初始化node demo

--- src/add.test.ts
+++
@@ -1,9 +0,0 @@
-import { add } from './add'
-
-test('two plus two is four', () => {
-    expect(add(2, 2)).toBe(4)
-})
-
-test('two plus two is not six', () => {
-    expect(add(2, 2)).not.toBe(6)
-})
--- src/add.ts
+++
@@ -1,3 +0,0 @@
-export function add(item: number, ...rest: number[]) {
-    return rest.reduce((a: number, b: number) => a + b, item)
-}
